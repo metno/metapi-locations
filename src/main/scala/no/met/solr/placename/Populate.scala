2d7b49ceeb458d3d162dce83b70f48618d68deef
Børge Moe <borge.moe@met.no>
2015-06-22 08:47:03 +0200

    Classes to populate ands query the solr placename database.

---
+++ src/main/scala/no/met/solr/placename/Populate.scala
@@ -0,0 +1,117 @@
+/*
+    MET-API
+
+    Copyright (C) 2014 met.no
+    Contact information:
+    Norwegian Meteorological Institute
+    Box 43 Blindern
+    0313 OSLO
+    NORWAY
+    E-mail: met-api@met.no
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+    MA 02110-1301, USA
+*/
+
+package no.met.solr.placename
+
+import java.io.{ File, Reader, FileReader }
+import scala.util.{ Try, Failure, Success }
+import play.api.libs.json._
+import no.met.fileutil._
+import no.met.fileutil.FileHelper.fileIter
+import no.met.placename._
+import no.met.placename.JsonWrites._
+import no.met.placename.JsonReads._
+import java.nio.file.FileSystems
+import scala.collection.mutable.ListBuffer
+import scala.util.Failure
+import java.io.FileReader
+import scala.util.matching.Regex
+import no.met.json.JsonHelper
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Failure
+import scala.util.Failure
+
+object Populate {
+  class SolrError(msg: String) extends RuntimeException(msg) {}
+
+  def doPopulate(solr: SolrPlacenameFeed, file: String, dryRun: Boolean = false,
+    maxElements: Long = Long.MaxValue): Try[Long] = Try {
+    var toMannyErrorsCount = 0;
+    var lastErrorMsg = ""
+    var count: Long = 0
+    var max: Long = 0
+    val lb = ListBuffer[PlacenameFeature]()
+
+    class MaxException extends RuntimeException {}
+
+    def maxProccessed: Unit = {
+      count += 1
+      max += 1
+      if (max > maxElements)
+        throw new MaxException()
+    }
+
+    def dryrun(v: JsValue, path: String): Unit = {
+      maxProccessed
+      val place = v.validate[PlacenameFeature]
+      val js = Json.toJson(place.get)
+      println(path + s"\n${"-" * path.length()}\n" + Json.prettyPrint(js) + "\n")
+    }
+
+    def jsParse(in: java.io.Reader, jsonPath: JsonHelper.Path, f: (JsValue, String) => Unit): Try[Unit] = {
+      JsonHelper.Parser.parse(in, jsonPath, f) recover {
+        case _: MaxException => ()
+        case e => throw e
+      }
+    }
+
+    def index(v: JsValue, path: String): Unit = v.validate[PlacenameFeature] match {
+      case JsSuccess(place, _) => solr.addPlacename(place) match {
+        case Failure(e) =>
+          println(s"Failed to index document(s) '$path'. Reason: ${e.getMessage}")
+          val msg = e.getMessage
+          toMannyErrorsCount += 1
+
+          if (msg == lastErrorMsg)
+            throw new SolrError(msg)
+          else if (toMannyErrorsCount > 10)
+            throw new SolrError(s"To manny errors: Last error '$msg'")
+
+          lastErrorMsg = msg
+        case _ => maxProccessed
+      }
+      case _ => println(s"Validation error: '$path'.[$v]")
+    }
+
+    val func = if (dryRun) dryrun _ else index _
+    val dataPath = JsonHelper.Path.parse("/features/@")
+
+    fileIter(file, ".*\\.geojson$".r) match {
+      case Success(iter) =>
+        for (reader <- iter) {
+          jsParse(reader, dataPath.get, func) match {
+            case Failure(e) =>
+              throw e
+            case _ =>
+          }
+          if (!dryRun) solr.commitDocuments(true)
+        }
+      case Failure(e) => throw e
+    }
+
+    count
+  }
+}
---
+++ src/main/scala/no/met/solr/placename/SolrPlacenameFeed.scala
@@ -0,0 +1,84 @@
+/*
+    MET-API
+
+    Copyright (C) 2015 met.no
+    Contact information:
+    Norwegian Meteorological Institute
+    Box 43 Blindern
+    0313 OSLO
+    NORWAY
+    E-mail: met-api@met.no
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+    MA 02110-1301, USA
+*/
+
+package no.met.solr.placename
+
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.client.solrj.impl.{ HttpSolrClient, CloudSolrClient }
+import org.apache.solr.common.SolrInputDocument
+import scala.collection.mutable.{ HashMap, HashSet }
+import scala.util.{ Try, Success, Failure }
+import play.api.libs.json._
+import play.api.libs.json.Json
+import play.api.libs.functional.syntax._
+import java.nio.file._
+import no.met.json._
+import no.met.placename._
+import no.met.placename.JsonWrites._
+import org.apache.solr.client.solrj.response.UpdateResponse
+import scala.util.{Failure, Success}
+
+/**
+ * Class to populate a Solr database with kartverkets placenames database that
+ * is distributed in geojson format.
+ *
+ * The schema is defined in src/solr_home/placenames/conf/solrconfig.xml.
+ */
+
+class SolrPlacenameFeed(solr: no.met.solr.SClient, cacheDocumentsBeforeSend: Integer = 100) {
+  private var countCache = 0
+  private var count = 0
+
+  def docsSendt = count
+  def newPlacename(place: PlacenameFeature): Try[SolrInputDocument] = Try {
+    val doc: SolrInputDocument = new SolrInputDocument()
+
+    doc.setField("ssrId", place.prop.enhSsrId)
+    doc.setField("ssrObjId", place.prop.enhSsrObjId)
+    doc.setField("name_type", place.prop.enhNavntype)
+    doc.setField("placename", place.prop.enhSnavn)
+    doc.setField("language", place.prop.enhSnspraak)
+    doc.setField("location", place.geometry.geom.asWkt)
+    doc.setField("jsonObj", Json.toJson(place))
+    doc
+  }
+
+  def commitDocuments(flush: Boolean = false): Try[Unit] = Try {
+    if (countCache > cacheDocumentsBeforeSend || (flush && countCache > 0)) {
+      val res = solr.client.commit( solr.collection )
+      count += countCache
+      countCache = 0
+    }
+    ()
+  }
+
+  def addDocument(doc: SolrInputDocument): Try[Unit] = Try {
+    val res = solr.client.add(solr.collection, doc)
+    countCache += 1
+  }
+
+  def addPlacename(place: PlacenameFeature): Try[Unit] =
+    newPlacename(place).flatMap(addDocument(_)).flatMap(_ => commitDocuments())
+}
