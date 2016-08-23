210f1f94d20072ee7ed7402a947e84f7a84cc6ea
Michael Akinde <michaeloa@met.no>
2016-08-24 00:49:58 +0200

    Fixed sql typo.

--- conf/evolutions/locations/3.sql
+++ conf/evolutions/locations/3.sql
@@ -1,8 +1,8 @@
 # --- !Ups
 
-INSERT INTO featureType VALUES (100, "City or large town");
-INSERT INTO featureType VALUES (132, "Part of a city");
-INSERT INTO featureType VALUES (101, "Small town");
+INSERT INTO featureType VALUES (100, 'City or large town');
+INSERT INTO featureType VALUES (132, 'Part of a city');
+INSERT INTO featureType VALUES (101, 'Small town');
 
 # --- !Downs
 
